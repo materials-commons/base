e10e2e81d6251ec7fc731e4bc1138bce3f5f4179
V. Glenn Tarcea <gtarcea@umich.edu>
2014-01-29 16:00:36 -0500

    Add in additional errors.

--- mc/errors.go
+++ mc/errors.go
@@ -5,7 +5,38 @@ import (
 )
 
 var (
-	ErrNotFound       = errors.New("Not found")
-	ErrInvalidRequest = errors.New("Invalid Request")
-	ErrExists         = errors.New("Exists")
+	ErrNotFound = errors.New("Not found")
+	ErrInvalid  = errors.New("Invalid")
+	ErrExists   = errors.New("Exists")
 )
+
+type ErrorCode int
+
+const (
+	ErrorCodeSuccess ErrorCode = iota
+	ErrorCodeNotFound
+	ErrorCodeInvalid
+	ErrorCodeExists
+	ErrorCodeUnknown
+)
+
+var errorCodeMapping = map[ErrorCode]error{
+	ErrorCodeSuccess:  nil,
+	ErrorCodeNotFound: ErrNotFound,
+	ErrorCodeInvalid:  ErrInvalid,
+	ErrorCodeExists:   ErrExists,
+}
+
+func ErrorCodeToError(ec ErrorCode) error {
+	return errorCodeMapping[ec]
+}
+
+var errorMapping = map[string]ErrorCode{
+	ErrNotFound.Error(): ErrorCodeNotFound,
+	ErrInvalid.Error():  ErrorCodeInvalid,
+	ErrExists.Error():   ErrorCodeInvalid,
+}
+
+func ErrorToErrorCode(err error) ErrorCode {
+	return errorMapping[err.Error()]
+}
