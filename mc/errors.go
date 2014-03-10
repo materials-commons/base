dd92c8e41417a09a354d82db8cadc1d45312770a
V. Glenn Tarcea <gtarcea@umich.edu>
2014-03-10 12:03:33 -0400

    Updates to pass golint.

--- mc/errors.go
+++ mc/errors.go
@@ -5,24 +5,52 @@ import (
 )
 
 var (
-	ErrNotFound = errors.New("Not found")
-	ErrInvalid  = errors.New("Invalid")
-	ErrExists   = errors.New("Exists")
-	ErrNoAccess = errors.New("No access")
-	ErrCreate   = errors.New("Unable to create")
-	ErrInternal = errors.New("Internal error")
+	// ErrNotFound Item not found
+	ErrNotFound = errors.New("not found")
+
+	// ErrInvalid Invalid request
+	ErrInvalid = errors.New("invalid")
+
+	// ErrExists Items already exists
+	ErrExists = errors.New("exists")
+
+	// ErrNoAccess Access to item not allowed
+	ErrNoAccess = errors.New("no access")
+
+	// ErrCreate Create failed reason unknown
+	ErrCreate = errors.New("unable to create")
+
+	// ErrInternal Internal fatal error
+	ErrInternal = errors.New("internal error")
 )
 
+// ErrorCode is an integer representation of a error that we can encode and send
+// over the network.
 type ErrorCode int
 
 const (
+	// ErrorCodeSuccess err == nil
 	ErrorCodeSuccess ErrorCode = iota
+
+	// ErrorCodeNotFound ErrNotFound
 	ErrorCodeNotFound
+
+	// ErrorCodeInvalid ErrInvalid
 	ErrorCodeInvalid
+
+	// ErrorCodeExists ErrCodeExists
 	ErrorCodeExists
+
+	// ErrorCodeNoAccess ErrNoAccess
 	ErrorCodeNoAccess
+
+	// ErrorCodeCreate ErrCreate
 	ErrorCodeCreate
+
+	// ErrorCodeInternal ErrInternal
 	ErrorCodeInternal
+
+	// ErrorCodeUnknown Catch all when we can't map an error
 	ErrorCodeUnknown
 )
 
@@ -36,6 +64,7 @@ var errorCodeMapping = map[ErrorCode]error{
 	ErrorCodeInternal: ErrInternal,
 }
 
+// ErrorCodeToError maps a given ErrorCode to an error.
 func ErrorCodeToError(ec ErrorCode) error {
 	return errorCodeMapping[ec]
 }
@@ -49,6 +78,7 @@ var errorMapping = map[string]ErrorCode{
 	ErrInternal.Error(): ErrorCodeInternal,
 }
 
+// ErrorToErrorCode maps from an error to an ErrorCode.
 func ErrorToErrorCode(err error) ErrorCode {
 	return errorMapping[err.Error()]
 }
