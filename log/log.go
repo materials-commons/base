83887532fe8a28be4c1f8ae7ef2a9ab431ebe93b
V. Glenn Tarcea <gtarcea@umich.edu>
2014-03-18 13:56:58 -0400

    Fix up golint errors.

--- log/log.go
+++ log/log.go
@@ -5,16 +5,19 @@ import (
 	"io"
 )
 
+// Global log variable.
 var L = logrus.New()
 
 func init() {
 	L.Formatter = new(logrus.JSONFormatter)
 }
 
+// SetOutput sets the output for the global logger.
 func SetOutput(out io.Writer) {
 	L.Out = out
 }
 
+// Field is a convenience function to create a single field that is used in a file or package.
 func Field(name, value string) logrus.Fields {
 	return logrus.Fields{
 		name: value,
