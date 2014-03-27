3a466053e9cbdbffb70957582c9886063957dc54
V. Glenn Tarcea <gtarcea@umich.edu>
2014-03-27 10:55:21 -0400

    Move dir package from gohandy to base since it really is specific to handling materials commons projects.

---
+++ dir/patch.go
@@ -0,0 +1,23 @@
+package dir
+
+// PatchType denotes the kind of patch operation
+type PatchType int
+
+const (
+	// PatchCreate created item
+	PatchCreate PatchType = iota
+
+	// PatchDelete deleted item
+	PatchDelete
+
+	// PatchEdit item content was changed
+	PatchEdit
+)
+
+// Patch is an instance of a difference when comparing two directories. It specifies
+// the kind of change to apply. The list of patches implies changes to apply to
+// the original directory to make it look like the new directory.
+type Patch struct {
+	File *FileInfo
+	Type PatchType
+}
