f937038640026f9ff68fc8c90f081db1df36ddaa
V. Glenn Tarcea <gtarcea@umich.edu>
2014-05-08 16:43:07 -0400

    Start cleaning up schema.

--- schema/dir.go
+++ schema/dir.go
@@ -7,32 +7,24 @@ import (
 // Directory models a directory of user files. A dir is an abstract representation
 // of a users file system directory plus the metadata needed by the system.
 type Directory struct {
-	ID              string    `gorethink:"id,omitempty"`
-	Access          string    `gorethink:"access" db:"-"`
-	Owner           string    `gorethink:"owner" db:"-"`
-	MarkedForReview bool      `gorethink:"marked_for_review" db:"-"`
-	Name            string    `gorethink:"name"`
-	DataFiles       []string  `gorethink:"datafiles" db:"-"`
-	DataParams      []string  `gorethink:"dataparams" db:"-"`
-	Users           []string  `gorethink:"users" db:"-"`
-	Tags            []string  `gorethink:"tags" db:"-"`
-	MyTags          []string  `gorethink:"mytags" db:"-"`
-	Parent          string    `gorethink:"parent"`
-	Reviews         []string  `gorethink:"reviews" db:"-"`
-	Birthtime       time.Time `gorethink:"birthtime"`
-	MTime           time.Time `gorethink:"mtime"`
-	ATime           time.Time `gorethink:"atime"`
+	ID        string    `gorethink:"id,omitempty"`
+	Owner     string    `gorethink:"owner"`
+	Name      string    `gorethink:"name"`
+	Project   string    `gorethink:"project"`
+	Parent    string    `gorethink:"parent"`
+	Birthtime time.Time `gorethink:"birthtime"`
+	MTime     time.Time `gorethink:"mtime"`
+	ATime     time.Time `gorethink:"atime"`
 }
 
 // NewDirectory creates a new Directory instance.
-func NewDirectory(name, access, owner, parent string) Directory {
+func NewDirectory(name, owner, project, parent string) Directory {
 	now := time.Now()
 	return Directory{
 		Owner:     owner,
 		Name:      name,
+		Project:   project,
 		Parent:    parent,
-		Access:    access,
-		Users:     []string{owner},
 		Birthtime: now,
 		MTime:     now,
 		ATime:     now,
--- schema/file.go
+++ schema/file.go
@@ -8,39 +8,26 @@ import (
 // a real file plus the attributes that we need in our model for access, and other
 // metadata.
 type File struct {
-	ID              string    `gorethink:"id,omitempty"`
-	Name            string    `gorethink:"name"`
-	Access          string    `gorethink:"access" db:"-"`
-	MarkedForReview bool      `gorethink:"marked_for_review" db:"-"`
-	Reviews         []string  `gorethink:"reviews" db:"-"`
-	Birthtime       time.Time `gorethink:"birthtime"`
-	MTime           time.Time `gorethink:"mtime"`
-	ATime           time.Time `gorethink:"atime"`
-	Tags            []string  `gorethink:"tags" db:"-"`
-	MyTags          []string  `gorethink:"mytags" db:"-"`
-	Description     string    `gorethink:"description" db:"-"`
-	Notes           []string  `gorethink:"notes" db:"-"`
-	Owner           string    `gorethink:"owner" db:"-"`
-	Process         string    `gorethink:"process" db:"-"`
-	Machine         string    `gorethink:"machine" db:"-"`
-	Checksum        string    `gorethink:"checksum"`
-	Size            int64     `gorethink:"size"`
-	Location        string    `gorethink:"location" db:"-"`
-	MediaType       string    `gorethink:"mediatype" db:"-"`
-	Conditions      []string  `gorethink:"conditions" db:"-"`
-	Text            string    `gorethink:"text" db:"-"`
-	MetaTags        []string  `gorethink:"metatags" db:"-"`
-	DataDirs        []string  `gorethink:"datadirs" db:"-"`
-	Parent          string    `gorethink:"parent"`
-	UsesID          string    `gorethink:"usesid" db:"-"`
+	ID          string    `gorethink:"id,omitempty"`
+	Name        string    `gorethink:"name"`
+	Birthtime   time.Time `gorethink:"birthtime"`
+	MTime       time.Time `gorethink:"mtime"`
+	ATime       time.Time `gorethink:"atime"`
+	Description string    `gorethink:"description"`
+	Notes       []string  `gorethink:"notes"`
+	Owner       string    `gorethink:"owner"`
+	Checksum    string    `gorethink:"checksum"`
+	Size        int64     `gorethink:"size"`
+	MediaType   string    `gorethink:"mediatype"`
+	Parent      string    `gorethink:"parent"`
+	UsesID      string    `gorethink:"usesid"`
 }
 
 // NewFile creates a new File instance.
-func NewFile(name, access, owner string) File {
+func NewFile(name, owner string) File {
 	now := time.Now()
 	return File{
 		Name:        name,
-		Access:      access,
 		Owner:       owner,
 		Description: name,
 		Birthtime:   now,
