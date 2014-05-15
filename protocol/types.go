0f99671ba3ce12e9661a1eae6d8acb445c34b441
V. Glenn Tarcea <gtarcea@umich.edu>
2014-05-15 16:41:40 -0400

    Cleanup comment.

--- protocol/types.go
+++ protocol/types.go
@@ -179,7 +179,7 @@ type DirectoryStatResp struct {
 	Entries     []StatEntry // A list of all the entries for this directory.
 }
 
-// SendBytesReq contains the bytes to write
+// SendBytesReq contains the bytes to write.
 type SendBytesReq struct {
 	FileID string // File ID on the server that these bytes are for
 	Bytes  []byte // The set of bytes to write
