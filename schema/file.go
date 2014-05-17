76807acbfc64d0b90e07450f69d8bc209b652816
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-05-17 14:33:13 -0400

    Add FileID method to determine which id to write to for a particular file.

--- schema/file.go
+++ schema/file.go
@@ -4,9 +4,8 @@ import (
 	"time"
 )
 
-// File models a user file. A datafile is an abstract representation of
-// a real file plus the attributes that we need in our model for access, and other
-// metadata.
+// File models a user file. A datafile is an abstract representation of a real file
+// plus the attributes that we need in our model for access, and other metadata.
 type File struct {
 	ID          string    `gorethink:"id,omitempty"`
 	Current     bool      `gorethink:"current"`
@@ -23,6 +22,17 @@ type File struct {
 	Parent      string    `gorethink:"parent"`
 	UsesID      string    `gorethink:"usesid"`
 	DataDirs    []string  `gorethink:"datadirs"`
+}
+
+// FileID returns the id to use for the file. Because files can be duplicates, all
+// duplicates are stored under a single ID. UsesID is set to the ID that an entry
+// points to when it is a duplicate.
+func (f *File) FileID() string {
+	if f.UsesID != "" {
+		return f.UsesID
+	}
+
+	return f.ID
 }
 
 // NewFile creates a new File instance.
